27b53fa5bb1df11f48dbdba0e94a2f004c0cec54
tastycai <realxiaojiu@163.com>
2019-06-30 22:45:10 +0800

    注册smsdda到eureka

--- sms/src/main/java/com/tastycai/sms/SmsApplication.java
+++ sms/src/main/java/com/tastycai/sms/SmsApplication.java
@@ -2,8 +2,10 @@ package com.tastycai.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SmsApplication {
 
     public static void main(String[] args) {
