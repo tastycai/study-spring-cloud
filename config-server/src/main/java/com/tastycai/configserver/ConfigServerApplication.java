99e5d6fd483ad8fa6e0861ec9765abba8a2276ae
tastycai <realxiaojiu@163.com>
2019-07-21 11:05:02 +0800

    init config-server

--- config-server/src/main/java/com/tastycai/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/tastycai/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.tastycai.config-server;
+package com.tastycai.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SmsApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
